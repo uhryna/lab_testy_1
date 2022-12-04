3d226c753300e87c1df59b485e13c89b37f91fd9
uhryna <63967823+uhryna@users.noreply.github.com>
2022-12-04 19:06:47 +0200

    Add files via upload

---
+++ AnalizerClassLibraryTests/AnalaizerClassTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using AnalaizerClassLibrary;
+
+namespace AnalizerClassLibrary.Tests
+{
+    [TestClass]
+    public class AnalaizerClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    "testData.xml",
+                    "Test", DataAccessMethod.Sequential)]
+        public void CheckCurrencyTest()
+        {
+            //Arrange
+            AnalaizerClass.expression = Convert.ToString(TestContext.DataRow["expression"]); // витягуємо дані з xml файлу
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expected"]);
+
+            //Act
+            bool result = AnalaizerClass.CheckCurrency(); // викликаємо метод для тестування
+
+            Assert.AreEqual(result, expected); // порівнюємо результат та очікуване значення
+        }
+    }
+}
---
+++ AnalizerClassLibraryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AnalizerClassLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnalizerClassLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("97f7aa27-2cdd-440f-ba82-26d4658ea89b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
